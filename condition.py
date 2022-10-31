39cf99ecb3a803cdf39041e46cd81caaf0e2d0a6
Özgür Özcan <98483468+ozgrozcn@users.noreply.github.com>
2022-10-31 22:44:15 +0300

    Update condition.py

--- condition.py
+++ condition.py
@@ -1,7 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 from datetime import datetime
 tm = datetime.strftime(datetime.today(),'%d')
 
-if int(tm) > 15: print("bööö")
+if int(tm) > 15: print("bööö :*")
 else: 
   for i in range(int(tm),0,-1): 
     print("{}. day is invalid".format(i))
