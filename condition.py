cefbff266ca5bbbc936c429ea71f9884b4bf2f38
Özgür Özcan <98483468+ozgrozcn@users.noreply.github.com>
2022-10-31 22:48:06 +0300

    Update condition.py

--- condition.py
+++ condition.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
 tm = datetime.strftime(datetime.today(),'%d')
-print(tm)
+print(datetime.strftime(datetime.today(),'%Y-%m-%d'))
 if int(tm) > 31: print("bööö :*")
 else: 
   for i in range(int(tm),0,-1): 
